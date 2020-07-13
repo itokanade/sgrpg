6d10f6ad65f45476868503f1317e7a678782db8d
Ito Kanade <kanade.ito01069@gmail.com>
2020-07-13 16:22:50 +0900

    commit

--- api/gacha/drop.php
+++ api/gacha/drop.php
@@ -7,151 +7,80 @@
 // 以下のコメントを外すと実行時エラーが発生した際にエラー内容が表示される
 // ini_set('display_errors', 'On');
 // ini_set('error_reporting', E_ALL);
-require_once("../Util.php");
-//-------------------------------------------------
-// 定数
-//-------------------------------------------------
-// キャラクター数
-define('MAX_CHARA', 10);
 
-// ガチゃ1回の価格
-define('GACHA_PRICE', 300);
-
+//-------------------------------------------------
+// ライブラリ
+//-------------------------------------------------
+require_once('../util.php');
+require_once('../../model/user.php');
+require_once('../../model/gacha.php');
+require_once('../../model/chara.php');
 
 //-------------------------------------------------
 // 引数を受け取る
 //-------------------------------------------------
-// ユーザーIDを受け取る
-$uid = isset($_GET['uid'])?  $_GET['uid']:null;
+$token = UserModel::getTokenfromQuery();
 
-// Validation
-if( ($uid === null) || (!is_numeric($uid)) ){
-  sendResponse(false, 'Invalid uid');
+if( !$token ){
+  sendResponse(false, 'Invalid token');
   exit(1);
 }
-
-//-------------------------------------------------
-// 準備
-//-------------------------------------------------
-//$dsn  = 'mysql:dbname=sgrpg2;host=127.0.0.1';  // 接続先を定義
-//$user = 'senpai';      // MySQLのユーザーID
-//$pw   = 'indocurry';   // MySQLのパスワード
-
-//---------------------------
-// 実行したいSQL
-//---------------------------
-// Userテーブルから所持金を取得
-$sql1 = 'SELECT money FROM User WHERE id=:userid';
-
-// Userテーブルの所持金を減産
-$sql2 = 'UPDATE User SET money=money-:price WHERE id=:userid';
-
-// UserCharaテーブルにキャラクターを追加
-$sql3 = 'INSERT INTO UserChara(user_id, chara_id) VALUES(:userid,:charaid)';
-
-// Charaテーブルから1レコード取得
-$sql4 = 'SELECT * FROM Chara WHERE id=:charaid';
 
 
 //-------------------------------------------------
 // SQLを実行
 //-------------------------------------------------
+$gacha = new GachaModel();
+$chara_id = $gacha->drop();  // 抽選のみ
+
 try{
-$dsn = Util::$dsn;
-  $user = Util::$user;
-  $pw = Util::$pw;
-  $dbh = new PDO($dsn, $user, $pw);   // 接続
-  $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);  // エラーモード
+  $user = new UserModel();
 
-  // トランザクション開始
-  $dbh->beginTransaction();
-
-  //---------------------------
-  // 所持金の残高を取得
-  //---------------------------
-  $sth = $dbh->prepare($sql1);
-  $sth->bindValue(':userid', $uid, PDO::PARAM_INT);
-  $sth->execute();
-  $buff = $sth->fetch(PDO::FETCH_ASSOC);
-
-  // ユーザーが存在しているかチェック
-  if( $buff === false ){
+  // ユーザーIDを取得
+  $uid = $user->getUserIdByToken($token);
+  if( $uid === false ){
     sendResponse(false, 'Not Found User');
     exit(1);
   }
 
-  // 残高が足りているかチェック
-  if( $buff['money'] < GACHA_PRICE ){
-    sendResponse(false, 'The balance is not enough');
+  // トランザクション開始
+  $user->begin();
+
+  // お金を消費
+  $ret = $user->useMoney($uid, GachaModel::$PRICE);
+  if( $ret === false ){
+    sendResponse(false, $user->getError());
     exit(1);
   }
 
-  //---------------------------
-  // 残高を減らす
-  //---------------------------
-  $sth = $dbh->prepare($sql2);
-  $sth->bindValue(':price', GACHA_PRICE, PDO::PARAM_INT);
-  $sth->bindValue(':userid', $uid,        PDO::PARAM_INT);
-  $sth->execute();
-
-  //---------------------------
-  // キャラクターを抽選
-  //---------------------------
-  $charaid = random_int(1, MAX_CHARA);
-
-  //---------------------------
-  // キャラクターを所有
-  //---------------------------
-  $sth = $dbh->prepare($sql3);
-  $sth->bindValue(':userid',  $uid,     PDO::PARAM_INT);
-  $sth->bindValue(':charaid', $charaid, PDO::PARAM_INT);
-  $sth->execute();
-
-  //---------------------------
-  // キャラクター情報を取得
-  //---------------------------
-  $sth = $dbh->prepare($sql4);
-  $sth->bindValue(':charaid', $charaid, PDO::PARAM_INT);
-  $sth->execute();
-  $chara = $sth->fetch(PDO::FETCH_ASSOC);
-
-  //---------------------------
-  // トランザクション確定
-  //---------------------------
-  $dbh->commit();
+  // キャラクターを所有  
+  $user->addChara($uid, $chara_id);
+  $user->commit();
 }
 catch( PDOException $e ) {
-  // ロールバック
-  $dbh->rollBack();
-
-  sendResponse(false, 'Database error: '.$e->getMessage());  // 本来エラーメッセージはサーバ内のログへ保存する(悪意のある人間にヒントを与えない)
+  $user->rollback();
+  sendResponse(false, 'Database error1: '.$e->getMessage());
   exit(1);
 }
 
 //-------------------------------------------------
 // 実行結果を返却
 //-------------------------------------------------
+try{
+  $chara = new CharaModel();
+  $buff = $chara->getRecordById($chara_id);
+}
+catch( PDOException $e ) {
+  sendResponse(false, 'Database error2: '.$e->getMessage());
+  exit(1);
+}
+
 // データが0件
 if( $buff === false ){
   sendResponse(false, 'System Error');
 }
 // データを正常に取得
 else{
-  sendResponse(true, $chara);
+  sendResponse(true, $buff);
 }
 
-
-/**
- * 実行結果をJSON形式で返却する
- *
- * @param boolean $status
- * @param array   $value
- * @return void
- */
-function sendResponse($status, $value=[]){
-  header('Content-type: application/json');
-  echo json_encode([
-    'status' => $status,
-    'result' => $value
-  ]);
-}
--- api/user/data.php
+++ api/user/data.php
@@ -1,65 +1,41 @@
 <?php
 /**
- * MySQLに接続しデータを追加する
+ * MySQLに接続しデータを取得する
  *
  */
 
 // 以下のコメントを外すと実行時エラーが発生した際にエラー内容が表示される
- //ini_set('display_errors', 'On');
- //ini_set('error_reporting', E_ALL);
-
-require_once("../Util.php");
-//alobal $pw;
-//-------------------------------------------------
-// 初期値
-//-------------------------------------------------
-define('DEFAULT_LV', 1);
-define('DEFAULT_EXP', 1);
-define('DEFAULT_MONEY', 3000);
+// ini_set('display_errors', 'On');
+// ini_set('error_reporting', E_ALL);
 
 //-------------------------------------------------
-// 準備
+// ライブラリ
 //-------------------------------------------------
+require_once("../util.php");
+require_once("../../model/user.php");
 
+//-------------------------------------------------
+// 引数を受け取る
+//-------------------------------------------------
+$token = UserModel::getTokenfromQuery();
 
-// 実行したいSQL
-$sql1 = 'INSERT INTO User(lv, exp, money) VALUES(:lv, :exp, :money)';
-$sql2 = 'SELECT LAST_INSERT_ID() as id';  // AUTO INCREMENTした値を取得する
-
+if( !$token ){
+  sendResponse(false, 'Invalid token');
+  exit(1);
+}
 
 //-------------------------------------------------
 // SQLを実行
 //-------------------------------------------------
 try{
-  $dsn = Util::$dsn;
-  $user = Util::$user;
-  $pw = Util::$pw;
-  $dbh = new PDO($dsn, $user, $pw);   // 接続
-  $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);  // エラーモード
-
-  //-------------------------------------------------
-  // 新規にレコードを作成
-  //-------------------------------------------------
-  // SQL準備
-  $sth = $dbh->prepare($sql1);
-  $sth->bindValue(':lv',    DEFAULT_LV,    PDO::PARAM_INT);
-  $sth->bindValue(':exp',   DEFAULT_EXP,   PDO::PARAM_INT);
-  $sth->bindValue(':money', DEFAULT_MONEY, PDO::PARAM_INT);
-
-  // 実行
-  $sth->execute();
-
-  //-------------------------------------------------
-  // AUTO INCREMENTした値を取得
-  //-------------------------------------------------
-  // SQL準備
-  $sth = $dbh->prepare($sql2);
-
-  // 実行
-  $sth->execute();
-
-  // 実行結果から1レコード取ってくる
-  $buff = $sth->fetch(PDO::FETCH_ASSOC);
+  $user = new UserModel();
+  $uid  = $user->getUserIdByToken($token);
+  if( $uid !== false ){
+    $buff = $user->getRecordById($uid);
+  }
+  else{
+    $buff = false;
+  }
 }
 catch( PDOException $e ) {
   sendResponse(false, 'Database error: '.$e->getMessage());  // 本来エラーメッセージはサーバ内のログへ保存する(悪意のある人間にヒントを与えない)
@@ -71,24 +47,10 @@ catch( PDOException $e ) {
 //-------------------------------------------------
 // データが0件
 if( $buff === false ){
-  sendResponse(false, 'Database error: can not fetch LAST_INSERT_ID()');
+  sendResponse(false, 'Not Found user');
 }
 // データを正常に取得
 else{
-  sendResponse(true, $buff['id']);
+  sendResponse(true, $buff);
 }
 
-/**
- * 実行結果をJSON形式で返却する
- *
- * @param boolean $status
- * @param array   $value
- * @return void
- */
-function sendResponse($status, $value=[]){
-  header('Content-type: application/json');
-  echo json_encode([
-    'status' => $status,
-    'result' => $value
-  ]);
- }
